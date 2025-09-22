0ce141c5860601fe10a9b576797b3d94b3fc0bf1
DESKTOP-5FBE16T\User <intelhsh@naver.com>
2025-09-22 13:41:08 +0900

    test: id manager red case 생성

--- mission2/test/test_player_manager.py
+++ mission2/test/test_player_manager.py
@@ -4,4 +4,7 @@ from ..src.player_manager import IdManager
 @pytest.mark.parametrize("id", [1, 3, 5, 7, 100, 5000])
 def test_player_number(id: int):
     id_mgr = IdManager()
-    assert False
+
+    for i in range(id - 1):
+        id_mgr.get_number()
+    assert id == id_mgr.get_number()
