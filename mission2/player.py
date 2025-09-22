528da1c733a53f96e9a0d6ef0ed75009e2ee67ca
DESKTOP-5FBE16T\User <intelhsh@naver.com>
2025-09-22 13:12:45 +0900

    refactor: Player class 정의

---
+++ mission2/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def init(self):
+        name = None
+        number = 0
+        attended = [0, 0, 0, 0, 0, 0, 0]
+        score = 0
+        grade = None
+        #         players[name] = {
+        #     "number": player_number,
+        #     "attended": [0, 0, 0, 0, 0, 0, 0],
+        #     "score": 0,
+        #     "grade": None,
+        # }
